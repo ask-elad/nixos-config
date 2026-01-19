bbba9f6bde9d227d33c0db0a5b021c965d7f3a95
ask-elad <aryanrai746@gmail.com>
2026-01-20 01:34:03 +0530

    idk

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -89,10 +89,10 @@
 
   # Enable the X11 windowing system
   services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.libinput.enable = true;
 
   # # Enable the KDE Plasma Desktop Environment
-  services.displayManager.sddm.enable = true;
-  services.libinput.enable = true;
   # services.desktopManager.plasma6.enable = true;
 
   # # Configure keymap in X11
