552f948184505e8410e82873c9ea2128832a5030
ask-elad <aryanrai746@gmail.com>
2026-01-20 01:43:44 +0530

    added waybar

--- modules/home-manager/sway.nix
+++ modules/home-manager/sway.nix
@@ -1,6 +1,25 @@
 { config, pkgs, ... }:
+
 {
   wayland.windowManager.sway = {
     enable = true;
+
+    config = {
+      modifier = "Mod4"; # Super / Windows key
+
+      terminal = "alacritty";
+
+      keybindings = {
+        "Mod4+Return" = "exec alacritty";
+        "Mod4+d" = "exec fuzzel";
+        "Mod4+Shift+e" = "exit";
+      };
+
+      bars = [ ]; # IMPORTANT: disable swaybar (we use waybar)
+    };
+
+    extraConfig = ''
+      exec waybar
+    '';
   };
 }
